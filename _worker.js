409ec863f7b1b79e065e66926e23123b96bdb06e
Lyrienjr1 <lyrienjr1@gmail.com>
2024-11-30 12:31:21 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '87838a45-626d-4bfa-81b7-c10e5898c368';
 
-const พร็อกซีไอพีs = ['91.192.81.154'];
+const พร็อกซีไอพีs = ['172.104.170.159'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
